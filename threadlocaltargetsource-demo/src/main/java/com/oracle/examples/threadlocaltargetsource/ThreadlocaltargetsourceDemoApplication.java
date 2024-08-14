c4ee704d3caaae1259a750acd530379cc8bb904b
Joydeep Sen <mailjoydeepsen@gmail.com>
2024-08-14 16:03:29 +0530

    Initial Commit.

---
+++ threadlocaltargetsource-demo/src/main/java/com/oracle/examples/threadlocaltargetsource/ThreadlocaltargetsourceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.examples.threadlocaltargetsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadlocaltargetsourceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadlocaltargetsourceDemoApplication.class, args);
+	}
+
+}
---
+++ threadlocaltargetsource-demo/src/test/java/com/oracle/examples/threadlocaltargetsource/ThreadlocaltargetsourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.examples.threadlocaltargetsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadlocaltargetsourceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
